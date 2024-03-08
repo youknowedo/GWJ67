e1198b99f69311e9f4ff016ab30cdf1b45ae5e1e
sqiddzzy <haglundaw@gmail.com>
2024-03-08 23:28:09 +0100

    Nästan klar med movement koden samt har lagt till en huvudkaraktär utan design

---
+++ Mainguy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var tile_size = 64
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+
+func _unhandled_input(event):
+	print("vadsom")
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	position += inputs[dir] * tile_size
