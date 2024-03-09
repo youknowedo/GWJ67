e914a91ded2962b10c8697d01b5503dc28b55397
Filip Mårtensson <fenwikk@protonmail.com>
2024-03-09 08:20:14 +0100

    Sprites and basic movement

--- Mainguy.gd
+++ Mainguy.gd
@@ -1,18 +1,48 @@
-extends Area2D
+extends CharacterBody2D
 
-var tile_size = 128
+@onready var ray = $RayCast2D
 
+@export var walk_speed = 6.0
+const TILE_SIZE = 16
+
+var initial_position = Vector2(0, 0)
+var input_direction = Vector2(0, 0)
+var is_moving = false
+var percent_moved_to_next_tile = 0.0
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	set_process_input(true)
-	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size / 2
+	initial_position = position
 
-func _input(_event):
-	if Input.is_action_pressed("ui_up"):
-		position.y -= tile_size
-	if Input.is_action_pressed("ui_down"):
-		position.y += tile_size
-	if Input.is_action_pressed("ui_left"):
-		position.x -= tile_size
-	if Input.is_action_pressed("ui_right"):
-		position.x += tile_size
+func _physics_process(delta):
+
+	if is_moving == false:
+		process_player_movement_input()
+	elif input_direction != Vector2.ZERO:
+		print(input_direction)
+		move(delta)
+	else:
+		is_moving = false
+
+func process_player_movement_input():
+	if input_direction.y == 0:
+		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	if input_direction.x == 0:
+		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+
+	input_direction.normalized()
+
+	ray.target_position = input_direction * TILE_SIZE
+	ray.force_raycast_update()
+	if !ray.is_colliding()&&input_direction != Vector2.ZERO:
+		initial_position = position
+		is_moving = true
+
+func move(delta):
+	percent_moved_to_next_tile += walk_speed * delta
+	if percent_moved_to_next_tile >= 1.0:
+		position = initial_position + (input_direction * TILE_SIZE)
+		percent_moved_to_next_tile = 0.0
+		is_moving = false
+	else:
+		position = initial_position + (input_direction * TILE_SIZE * percent_moved_to_next_tile)
