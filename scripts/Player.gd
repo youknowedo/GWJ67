76b2ff8f3221c9dcb31f59fff534f4cadb653a04
Filip Mårtensson <fenwikk@protonmail.com>
2024-03-09 10:14:26 +0100

    Player animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 signal player_move
 
 @onready var ray = $RayCast2D
-@export var walk_speed = 6.0
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
 
 const TILE_SIZE = 16
 
+@export var crawl_speed = 6.0
 var initial_position = Vector2(0, 0)
 var input_direction = Vector2(0, 0)
 var is_moving = false
@@ -21,9 +23,10 @@ func _physics_process(delta):
 	if is_moving == false:
 		process_player_movement_input()
 	elif input_direction != Vector2.ZERO:
-		print(input_direction)
+		anim_state.travel("Crawl")
 		move(delta)
 	else:
+		anim_state.travel("Idle")
 		is_moving = false
 
 func process_player_movement_input():
@@ -37,11 +40,15 @@ func process_player_movement_input():
 	ray.target_position = input_direction * TILE_SIZE
 	ray.force_raycast_update()
 	if !ray.is_colliding()&&input_direction != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_direction)
+		anim_tree.set("parameters/Crawl/blend_position", input_direction)
 		initial_position = position
 		is_moving = true
+	else:
+		anim_state.travel("Idle")
 
 func move(delta):
-	percent_moved_to_next_tile += walk_speed * delta
+	percent_moved_to_next_tile += crawl_speed * delta
 	if percent_moved_to_next_tile >= 1.0:
 		position = initial_position + (input_direction * TILE_SIZE)
 		percent_moved_to_next_tile = 0.0
