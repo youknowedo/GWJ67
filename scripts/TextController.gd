1b8b29a95da3b1e023735688b97f0639a8f34898
Filip Mårtensson <fenwikk@protonmail.com>
2024-03-17 18:42:19 +0100

    Select sound

--- scripts/TextController.gd
+++ scripts/TextController.gd
@@ -4,6 +4,8 @@ class_name TextController
 @export var text: Text
 @export var player: Player
 
+@onready var select: AudioStreamPlayer = $Select
+
 var text_up = false
 
 # Called when the node enters the scene tree for the first time.
@@ -17,5 +19,6 @@ func show_text(_text: String):
 
 func _process(delta):
 	if text_up&&Input.is_action_just_pressed("primary"):
+		select.play()
 		hide()
 		text_up = false
